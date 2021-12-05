fa9df7dbb1464700a095d1cb6cb59dad22ab2d9e
AndrewG <andrewalangm@gmail.com>
2021-12-05 14:05:48 -0600

    🤿 Day 02 - Dive!  - part 1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,36 @@
 defmodule AdventOfCode.Day02 do
+  def get_parsed_instructions(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn instruction ->
+      instruction_parts = instruction |> String.trim() |> String.split(" ")
+      {instruction_parts |> Enum.at(0), instruction_parts |> Enum.at(1) |> String.to_integer()}
+    end)
+  end
+
+  def calculate_step({x, y}, {"forward", steps}) do
+    {x + steps, y}
+  end
+
+  def calculate_step({x, y}, {"up", steps}) do
+    {x, y + steps}
+  end
+
+  def calculate_step({x, y}, {"down", steps}) do
+    {x, y - steps}
+  end
+
+  def calculate_position(instructions) do
+    instructions
+    |> Enum.reduce({0, 0}, fn instruction, position ->
+      calculate_step(position, instruction)
+    end)
+  end
+
   def part1(args) do
+    {x, y} = args |> get_parsed_instructions() |> calculate_position()
+    abs(x) * abs(y)
   end
 
   def part2(args) do
