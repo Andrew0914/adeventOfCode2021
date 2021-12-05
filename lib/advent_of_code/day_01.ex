21c72d192442986692bebb4f5532ee299c0a546f
AndrewG <andrewalangm@gmail.com>
2021-12-05 14:01:43 -0600

    🔘 Day 01 - Sonar sweep - part 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,5 +1,30 @@
 defmodule AdventOfCode.Day01 do
+  def get_measurements(args) do
+    args
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(fn str ->
+      str |> String.trim() |> String.to_integer()
+    end)
+  end
+
+  def detect_increments(measurements) do
+    measurements
+    |> Enum.reduce({0, 0}, fn measurement, {index, acc} ->
+      if index <= 0 do
+        {index + 1, 0}
+      else
+        acc = if measurement > Enum.at(measurements, index - 1), do: acc + 1, else: acc + 0
+        {index + 1, acc}
+      end
+    end)
+    |> elem(1)
+  end
+
   def part1(args) do
+    args
+    |> get_measurements()
+    |> detect_increments()
   end
 
   def part2(args) do
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,40 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
 
-    assert result
+    result = part1(input)
+    assert result == 7
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
+
     result = part2(input)
 
-    assert result
+    assert result == 5
   end
 end
