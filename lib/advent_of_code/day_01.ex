bbac2d979dc7e15e8e669f87c711c7db0fb93d1e
AndrewG <andrewalangm@gmail.com>
2021-12-05 14:04:15 -0600

    🔘 Day 01 - Sonar sweep - part 2

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -27,6 +27,20 @@ defmodule AdventOfCode.Day01 do
     |> detect_increments()
   end
 
+  def calculate_trio_sums(measurements) do
+    measurements
+    |> Enum.reduce({0, []}, fn measurement, {index, acc} ->
+      value_one = Enum.at(measurements, index + 1)
+      value_two = Enum.at(measurements, index + 2)
+      value_one = if is_integer(value_one), do: value_one, else: 0
+      value_two = if is_integer(value_two), do: value_two, else: 0
+
+      {index + 1, acc ++ [measurement + value_one + value_two]}
+    end)
+    |> elem(1)
+  end
+
   def part2(args) do
+    args |> get_measurements() |> calculate_trio_sums() |> detect_increments()
   end
 end
